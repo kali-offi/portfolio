f8a472f53a0e489f95479fc1fa50a29a24deda62
Your Name <you@example.com>
2025-02-12 11:20:22 +0530

    first commit

--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -24,7 +24,7 @@ const Portfolio = () => {
                 <header className='d-flex justify-content-between align-items-center'>
                 <svg width="40" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
                 <div>
-                <a href="">
+                <a href="https://github.com/kali-offi/Go-Daddy/tree/main/project">
                     <img src="https://portfolio-5678.netlify.app/static/media/github-icon.c48bfb2a53c47ee345a50672e3d24e07.svg" className='img-fluid logo2' alt="" />
                 </a>
                 <a href="">
@@ -33,13 +33,12 @@ const Portfolio = () => {
                 </div>
                 </header>
                 <section>
-                    <h5 className='my-3'>E Commerce</h5>
+                    <h5 className='my-3'>Go-daddy Clone</h5>
                 
-                    <h6 className='mt-3' >A website that allows people to buy and sell physical goods, services, and digital products over the internet rather than at a brick-and-mortar location.</h6>
+                    <h6 className='mt-3' >A website that allows people to buy and sell Lands, services, and Modernate Lands over the internet rather than at a brick-and-mortar location.</h6>
                 </section>
                 <section className='used-apps d-flex justify-content-between px-2 '>
-                    <p className='text-secondary'>React</p>
-                    <p className='text-secondary'>JS</p>
+                    <p className='text-secondary'>HTML</p>
                     <p className='text-secondary'>Bootstrap</p>
                     <p className='text-secondary'>Css</p>
                 </section>
@@ -48,18 +47,18 @@ const Portfolio = () => {
                 <header className='d-flex justify-content-between align-items-center'>
                 <svg width="40" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
                 <div>
-                <a href="https://github.com/kavi307/Java-Script-">
+                <a href="https://github.com/kali-offi/ecommerce">
                     <img src="https://portfolio-5678.netlify.app/static/media/github-icon.c48bfb2a53c47ee345a50672e3d24e07.svg" className='img-fluid logo2' alt="" />
                 </a>
-                <a href="https://github.com/kavi307/Java-Script-">
+                <a href="">
                     <img src="	https://portfolio-5678.netlify.app/static/media/external-icon.97075d22dfb22a8c8d253b264e892b5f.svg" className='img-fluid logo2'  alt="" />
                 </a>
                 </div>
                 </header>
                 <section>
-                    <h5 className='my-3'>Chat App</h5>
+                    <h5 className='my-3'>Ecommerce</h5>
                 
-                    <h6 className='mt-3' >This app is an chat app allows the user to login and chat with other user who were loggedin. An online chat application is a software application that enables users to exchange messages and communicate with each other in real-time.</h6>
+                    <h6 className='mt-3' >This app is Purchase products and things Sells . Free Delivery in all are products</h6>
                 </section>
                 <section className='d-flex justify-content-between '>
                     <p className='text-secondary'>React</p>
@@ -72,7 +71,7 @@ const Portfolio = () => {
                 <header className='d-flex justify-content-between align-items-center'>
                 <svg width="40" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
                 <div>
-                <a href="">
+                <a href="https://github.com/kali-offi/portfolio">
                     <img src="https://portfolio-5678.netlify.app/static/media/github-icon.c48bfb2a53c47ee345a50672e3d24e07.svg" className='img-fluid logo2' alt="" />
                 </a>
                 <a href="">
@@ -81,9 +80,9 @@ const Portfolio = () => {
                 </div>
                 </header>
                 <section>
-                    <h5 className='my-3'>Pizza Delivery App</h5>
+                    <h5 className='my-3'>Portfolio</h5>
                 
-                    <h6 className='mt-3' >Access to different categories of pizza. This application is providing access to the customers over different categories of pizza.</h6>
+                    <h6 className='mt-3' >person working and insudtial related works documents collection create this portfolio.</h6>
                 </section>
                 <footer className='d-flex justify-content-between '>
                     <p className='text-secondary'>React</p>
@@ -97,7 +96,7 @@ const Portfolio = () => {
                 <header className='d-flex justify-content-between align-items-center'>
                 <svg width="40" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
                 <div>
-                <a href="">
+                <a href="https://github.com/kali-offi/bank_program_python">
                     <img src="https://portfolio-5678.netlify.app/static/media/github-icon.c48bfb2a53c47ee345a50672e3d24e07.svg" className='img-fluid logo2' alt="" />
                 </a>
                 <a href="">
@@ -106,15 +105,12 @@ const Portfolio = () => {
                 </div>
                 </header>
                 <section>
-                    <h5 className='my-3'>Blog App</h5>
+                    <h5 className='my-3'>Banking Program</h5>
                 
-                    <h6 className='mt-3' >A blog application is a software program that provides an online environment and features for a blogger.</h6>
+                    <h6 className='mt-3' >A bank programm in run time server. An Idle app using this project make it</h6>
                 </section>
                 <footer className='d-flex justify-content-between '>
-                    <p className='text-secondary'>React</p>
-                    <p className='text-secondary'>JS</p>
-                    <p className='text-secondary'>Bootstrap</p>
-                    <p className='text-secondary'>Css</p>
+                    <p className='text-secondary'>Python</p>
                 </footer>
                
             </div>
@@ -122,7 +118,7 @@ const Portfolio = () => {
                 <header className='d-flex justify-content-between align-items-center'>
                 <svg width="40" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
                 <div>
-                <a href="">
+                <a href="https://github.com/kali-offi/spotify-clone">
                     <img src="https://portfolio-5678.netlify.app/static/media/github-icon.c48bfb2a53c47ee345a50672e3d24e07.svg" className='img-fluid logo2' alt="" />
                 </a>
                 <a href="">
@@ -131,9 +127,9 @@ const Portfolio = () => {
                 </div>
                 </header>
                 <section>
-                    <h5 className='my-3'>Nationalize API App</h5>
+                    <h5 className='my-3'>Spotify Clone</h5>
                 
-                    <h6 className='mt-3' >This Nationalize.io predicts the nationality of a person given their name.The portal serves as one stop store for developers to discover and test Government APIs on a secure platform</h6>
+                    <h6 className='mt-3' >Any audios palying online app like Spotify same clone online entertainment app. Spotify clone apps cater to the growing demand for personalized audio-sharing platforms.</h6>
                 </section>
                 <footer className='d-flex justify-content-between '>
                     <p className='text-secondary'>React</p>
@@ -147,7 +143,7 @@ const Portfolio = () => {
                 <header className='d-flex justify-content-between align-items-center'>
                 <svg width="40" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
                 <div>
-                <a href="">
+                <a href="https://github.com/kali-offi/netflix">
                     <img src="https://portfolio-5678.netlify.app/static/media/github-icon.c48bfb2a53c47ee345a50672e3d24e07.svg" className='img-fluid logo2' alt="" />
                 </a>
                 <a href="">
@@ -156,14 +152,14 @@ const Portfolio = () => {
                 </div>
                 </header>
                 <section>
-                    <h5 className='my-3'>Youtube Clone</h5>
+                    <h5 className='my-3'>Netflix Clone</h5>
                 
-                    <h6 className='mt-3' >Any videos palying online app like youtube same clone online entertainment app.YouTube clone apps cater to the growing demand for personalized video-sharing platforms. </h6>
+                    <h6 className='mt-3' >Any videos palying online app like Netflix same clone online entertainment app.Netflix clone apps cater to the growing demand for personalized video-sharing platforms. </h6>
                 </section>
                 <footer className='d-flex justify-content-between '>
                     <p className='text-secondary'>React</p>
                     <p className='text-secondary'>JS</p>
-                    <p className='text-secondary'>Bootstrap</p>
+                    <p className='text-secondary'>Tailwind</p>
                     <p className='text-secondary'>Css</p>
                 </footer>
                
